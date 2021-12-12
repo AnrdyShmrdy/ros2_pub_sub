8b21a9e5b4769dc00cdf14a0095594160a042c63
Andy Ponce <andy.ponce@outlook.com>
2021-12-11 21:59:40 -0500

    added entry point and matched description and license tag to package.xml

--- py_pubsub/setup.py
+++ py_pubsub/setup.py
@@ -15,11 +15,12 @@ setup(
     zip_safe=True,
     maintainer='andyp',
     maintainer_email='andy.ponce@outlook.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Examples of minimal publisher/subscriber using rclpy',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+        	'talker = py_pubsub.publisher_member_function:main',
         ],
     },
 )
